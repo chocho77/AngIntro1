1bfbcff8dfaf5f4383b0571aef32035ecccb43c4
chavdar.momchilov <chavmom@gmail.com>
2023-05-01 12:55:02 +0300

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	modified:   src/app/navbar/navbar.conponent.ts

--- src/app/navbar/navbar.conponent.ts
+++ src/app/navbar/navbar.conponent.ts
@@ -2,7 +2,12 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'app-navbar',
-    template: '<h1> Navbar Component </h1>'
+    template: `<h1> Navbar Component </h1>
+               <p>lorem ipsum doller immet</p>`,
+    styles: [`h1{
+        color:red;
+        font-size: 50px
+    }`]
 
 })
 
